e5be996c96cba3dfc4ff947051f23763fcfde714
Fiona Wilson <fiona@razerfish.dev>
2020-12-13 18:40:19 -0500

    Third draft of homelist

--- home.js
+++ home.js
@@ -2,21 +2,22 @@
 // https://github.com/bmartins/tvmaze-js
 // https://www.tvmaze.com/api
 
-function initShortlist() {
+
+function createShortlist() {
     let shortlist = document.createElement("div");
 
-    shortlist.classList.add("w-100", "d-flex", "flex-row", "justify-content-between", "home-list");
+    shortlist.classList.add("w-100", "d-flex", "flex-row", "justify-content-between", "homelist");
     shortlist.id = "shortlist";
 
     document.getElementById("shortlist-container").appendChild(shortlist);
 }
 
 
-function initSchedule() {
+function createSchedule() {
     let schedule = document.createElement("div");
 
-    schedule.classList.add("w-100", "d-flex", "flex-row", "justify-content-around", "home-list");
-    shortlist.id = "schedule";
+    schedule.classList.add("w-100", "d-flex", "flex-row", "justify-content-between", "homelist");
+    schedule.id = "schedule";
 
     document.getElementById("schedule-container").appendChild(schedule);
 }
@@ -24,12 +25,12 @@ function initSchedule() {
 
 let tvmaze = new TVMaze();
 window.onload = () => {
-    initShortlist();
+    createShortlist();
 
-    displayShortlistThumbnail(document.getElementById("shortlist"), 178);
-    displayShortlistThumbnail(document.getElementById("shortlist"), 37089);
-    displayShortlistThumbnail(document.getElementById("shortlist"), 2071);
-    displayShortlistThumbnail(document.getElementById("shortlist"), 1121);
-    displayShortlistThumbnail(document.getElementById("shortlist"), 5460);
-    displayShortlistThumbnail(document.getElementById("shortlist"), 7557);
+    createHomelistItem(document.getElementById("shortlist"), 178);
+    createHomelistItem(document.getElementById("shortlist"), 37089);
+    createHomelistItem(document.getElementById("shortlist"), 2071);
+    createHomelistItem(document.getElementById("shortlist"), 1121);
+    createHomelistItem(document.getElementById("shortlist"), 5460);
+    createHomelistItem(document.getElementById("shortlist"), 7557);
 }
