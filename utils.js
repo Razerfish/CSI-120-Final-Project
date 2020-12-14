29a4649426af6c156503353a6f973c727a815e31
Fiona Wilson <fiona@razerfish.dev>
2020-12-14 17:27:42 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -12,3 +12,21 @@ function getWatchlist() {
         return JSON.parse(watchlist).watchlist;
     }
 }
+
+
+function addToWatchlist(id) {
+    let watchlist = getWatchlist();
+    watchlist.push(id);
+    saveWatchlist(watchlist);
+}
+
+
+function removeFromWatchlist(id) {
+    let watchlist = getWatchlist();
+
+    while (watchlist.indexOf(id) != -1) {
+        watchlist.splice(watchlist.indexOf(id), 1);
+    }
+
+    saveWatchlist(watchlist);
+}
